6ec275c93233d512c2b1cf9730880c7a83213506
Marc Backes <mab@mab.lu>
2019-06-28 09:59:22 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
-# random-letter
+# random-letter-picker
+
+A mini Vue application that picks a random letter and shows it after a 5 second countdown.
+
+## Why?
+
+### Stadt-Land-Fluss
+I don't know if other countries have this, but in Europe we have a game called "Stadt-Land-Fluss" (city, country, river).
+
+At the beginning, the players decide on the game categories. Originally: city, country and river. But it can be extended to any number of categories and can be more than 3. For example: food, celebrity, company, etc.
+
+### Rounds
+Each round goes as follows:
+ - One player counts letters A-Z in their head
+ - The player on the left of the first player says "stop" after a while
+ - First player then says out loud the letter he was currently counting
+ - Now the round starts!
+ - Each player writes as fast as they can an item in each category, starting with the chosen letter
+ - First player to fill in all categories says "stop" out loud
+ - All players stop writing (variation: they can finish the word they were writing)
+
+### Scores
+For each category, scores are being added:
+ - When one player is the only one who has a valid word of the category: 20 points
+ - When multiple players have a valid word of the category: 10 points
+ - **UNLESS**: More than one player have the same valid word, for those players: 5 points
+ - When a player has no valid word for the category: 0 points
+
+After all 26 rounds (1 per letter) are done, the points for each round is added per player.
+The player with the highest score wins.
+
+### Problems
+This is a fun game once in a while, but it has two major flaws:
+ - Cheating: The player counting in their head the letters can pre-think of a letter and prepare answers so he can finish fast
+ - Unconvenience: The first couple of rounds it's all good. But soon the letters repeat. And the letter counting has to proceed until an unused letter is found
+
+## Use
+
+This little application eliminates both [problems](#Problems) of the game.
+
+Using the app is easy:
+ - Click `Pick letter`
+ - The randomly chosen letter appears after 5 seconds
+
+To delete a letter from the used letter pool, click that letter.
+
+To restart the game, click `Restart`
+
+## Demo
+
+You can find a demo here:
+
+https://letterpicker.marc.dev
 
 ## Project setup
 ```
---
+++ src/components/RandomLetter.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="random-letter">
+        <div class="letter">
+            <span v-if="!countdown.active">{{ currentLetter }}</span>
+            <span v-else>{{ countdown.count }}</span>
+        </div>
+        <div v-if="letterPool.length > 0 && !countdown.active" class="button big" @click="pickLetter">Pick letter</div>
+        <div v-else class="button big disabled" @click="pickLetter">Pick letter</div>
+        <br>
+        <div class="button secondary" @click="newGame">Restart</div>
+
+        <div class="used-letters">
+            <div class="used-letter" v-for="letter in usedLetters" :key="letter" @click="returnToPool(letter)">{{letter}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            letterPool: localStorage.letterPool
+                ? JSON.parse(localStorage.letterPool)
+                : this.alphabeth(),
+
+            currentLetter: localStorage.currentLetter || "-",
+
+            // TODO: Put the logic of the countdown in a module
+            countdown: {
+                count: 0,
+                active: false,
+                start(callback) {
+                    this.count = 5;
+                    this.active = true;
+                    const interval = setInterval(() => {
+                        console.log(this.count);
+                        this.count -= 1;
+                        if (this.count == 0) {
+                            this.active = false;
+                            callback();
+                            clearInterval(interval);
+                        }
+                    }, 1000);
+                }
+            }
+        };
+    },
+    methods: {
+        pickLetter() {
+            const pick = this.letterPool[
+                Math.floor(Math.random() * this.letterPool.length)
+            ];
+            this.countdown.start(() => {
+                this.letterPool = this.letterPool.filter(
+                    letter => letter != pick
+                );
+                this.writeLocalStorage();
+                this.currentLetter = pick.toUpperCase();
+            });
+        },
+        alphabeth() {
+            return "abcdefghijklmnopqrstuvwxyz".split("");
+        },
+        newGame() {
+            this.letterPool = this.alphabeth();
+            this.currentLetter = "-";
+            this.writeLocalStorage();
+        },
+        writeLocalStorage() {
+            localStorage.letterPool = JSON.stringify(this.letterPool);
+            localStorage.currentLetter = this.currentLetter;
+        },
+        returnToPool(letter) {
+            this.letterPool.push(letter)
+            this.writeLocalStorage()
+        }
+    },
+    computed: {
+        usedLetters() {
+            return this.alphabeth().filter(
+                letter => !this.letterPool.includes(letter)
+            );
+        }
+    }
+};
+</script>
+
+<style>
+.letter {
+    font-size: 1700%;
+    font-weight: bold;
+}
+
+.button {
+    background: #2ecc71;
+    color: #fff;
+    padding: 10px 20px;
+    cursor: pointer;
+    border-radius: 8px;
+    display: inline-block;
+    margin: 5px;
+    font-weight: bold;
+}
+
+.button.disabled {
+    background: #bdc3c7;
+    opacity: 0.5;
+    pointer-events: none;
+}
+
+.button.big {
+    font-size: 200%;
+}
+
+.button.secondary {
+    background: #95a5a6;
+}
+
+.used-letters {
+    margin: 20px;
+    overflow-wrap: break-word;
+    line-height: 40px;
+}
+.used-letter {
+    background: #34495e;
+    color: #fff;
+    padding: 4px 8px;
+    margin: 5px;
+    border-radius: 3px;
+    display: inline;
+    text-transform: uppercase;
+    cursor: pointer;
+}
+
+@media only screen and (min-device-width: 900px) {
+    .used-letters {
+        line-height: 70px;
+    }
+    .used-letter {
+        font-size: 200%;
+        padding: 10px 20px;
+        border-radius: 8px;
+    }
+}
+@media only screen and (max-device-height: 900px) {
+    .letter {
+        font-size: 1300%;
+    }
+}
+
+@media only screen and (max-device-height: 600px) {
+    .letter {
+        font-size: 1000%;
+    }
+    .button.big {
+        font-size: 150%;
+    }
+}
+</style>
